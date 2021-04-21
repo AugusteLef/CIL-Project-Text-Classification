6187a01e54c40b78530e56fe730e415c8b351446
Tobias Birchler <tobiabir@student.ethz.ch>
2021-04-21 21:50:27 +0200

    working BERT-based implementation (currently on Imdb dataset but ready for twitter dataset)

--- main.py
+++ main.py
@@ -1,98 +1,94 @@
+import os
 import torch
 from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import AdamW
+from pathlib import Path
 
-PATH_DATA_NEG = "Data/train_neg_preprocessed.txt"
-PATH_DATA_POS = "Data/train_pos_preprocessed.txt"
-PRE_TRAINED_MODEL_NAME = "bert-base-uncased"
-MAX_SEQ_LEN = 98
-N_EPOCHS = 1
-BATCH_SIZE = 2
+DIR_DATA = "Data"
+PATH_DATA_NEG = os.path.join(DIR_DATA, "train_neg_preprocessed.txt")
+PATH_DATA_POS = os.path.join(DIR_DATA, "train_pos_preprocessed.txt")
+DIR_PRETRAINED = "Pretrained/bert-base-uncased"
+N_EPOCHS = 3
+BATCH_SIZE = 16
+ACCUMULATION_SIZE = 16
+VERBOSE = True
 
-class TwitterDataset(torch.utils.data.Dataset):
-
-    def __init__(self, tweets, labels, tokenizer, max_len):
-        """
-        TODO: comment
-        """
-        super().__init__()
-        self.tweets = tweets
+class EncodingsDataset(torch.utils.data.Dataset):
+    
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
         self.labels = labels
-        self.tokenizer = tokenizer
-        self.max_len = max_len
 
     def __len__(self):
-        """
-        :return: length of the dataset
-        """
-        return len(self.tweets)
+        return len(self.labels)
 
     def __getitem__(self, idx):
-        """
-        :param idx: index
-        :return: item at index idx
-        """
-        tweet = self.tweets[idx]
-        label = self.labels[idx]
-        encoding = self.tokenizer.encode_plus(
-            tweet,
-            add_special_tokens=True,
-            max_length=self.max_len,
-            return_token_type_ids=False,
-            pad_to_max_length=True,
-            return_attention_mask=True,
-            return_tensors='pt',
-        )
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
 
-        input_ids = encoding['input_ids'].flatten()
-        attention_mask = encoding['attention_mask'].flatten()
-        return input_ids, attention_mask, label
+def read_imdb_split(split_dir):
+    split_dir = Path(split_dir)
+    texts = []
+    labels = []
+    for label_dir in ["pos", "neg"]:
+        for text_file in (split_dir/label_dir).iterdir():
+            texts.append(text_file.read_text())
+            labels.append(0 if label_dir == "neg" else 1)
 
-class BertBinarySeqClassifier(torch.nn.Module):
-    
-    def __init__(self):
-        super().__init__()
-        self.bert_model = BertForSequenceClassification.from_pretrained(PRE_TRAINED_MODEL_NAME)
-        self.softmax = torch.nn.Softmax()
-    
-    def forward(self, input_ids, attention_mask):
-        tmp = self.bert_model(input_ids, attention_mask)
-        return self.softmax(tmp[0])
+    return texts, labels
 
 def main():
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if VERBOSE: print("reading data...")
+    #f_neg = open(PATH_DATA_NEG)
+    #texts_neg = f_neg.readlines()
+    #f_pos = open(PATH_DATA_POS)
+    #texts_pos = f_pos.readlines()
+    #texts = texts_neg + texts_pos
+    #labels = [0] * len(texts_neg) + [1] * len(texts_pos)
+    texts, labels = read_imdb_split(os.path.join(DIR_DATA, 'aclImdb/train'))
 
-    f_neg = open(PATH_DATA_NEG)
-    l_neg = f_neg.readlines()
-    f_pos = open(PATH_DATA_POS)
-    l_pos = f_pos.readlines()
-    l = l_neg + l_pos
-    labels = [0] * len(l_neg) + [1] * len(l_pos)
+    if VERBOSE: print("loading tokenizer...")
+    tokenizer = BertTokenizer.from_pretrained(DIR_PRETRAINED) 
 
-    tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME) 
-
-    ds = TwitterDataset(l, labels, tokenizer, MAX_SEQ_LEN) 
+    if VERBOSE: print("tokenizing data...")
+    encodings = tokenizer(texts, truncation=True, padding=True)
+    
+    ds = EncodingsDataset(encodings, labels) 
     dl = torch.utils.data.DataLoader(
         dataset=ds,
-        batch_size=BATCH_SIZE,
+        batch_size=int(BATCH_SIZE/ACCUMULATION_SIZE),
         shuffle=True,
     )
     
-    model = BertBinarySeqClassifier()
-    
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.01)
-    loss_fn = torch.nn.CrossEntropyLoss()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    model = BertForSequenceClassification.from_pretrained(DIR_PRETRAINED)
+    model = torch.nn.DataParallel(model)
+    model.to(device)
+    
+    optimizer = AdamW(model.parameters(), lr=5e-5)
+
+    if VERBOSE: print("training...")
     model.train()
     for epoch in range(N_EPOCHS):
-        for input_ids, attention_mask, label in dl:
-            pred = model(input_ids, attention_mask)
-            loss = loss_fn(pred, label)
-            print(loss)
-            optimizer.zero_grad()
+        if VERBOSE: print("epoch %d..." % epoch)
+        avg_loss = torch.tensor(0.0)
+        for i, batch in enumerate(dl):
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+            loss = outputs[0]
+            loss /= ACCUMULATION_SIZE
             loss.backward()
-            optimizer.step()
-            
+            avg_loss += loss.detach()
+            if (i + 1) % ACCUMULATION_SIZE == 0:
+                optimizer.step()
+                optimizer.zero_grad() 
+                print(avg_loss)
+                avg_loss = torch.tensor(0.0)
 
 if __name__ == "__main__":
     main()
