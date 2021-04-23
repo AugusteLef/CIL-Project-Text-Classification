af32e7ba997d1402e129ef9dd8de506934f363c7
Tobias Birchler <tobiabir@student.ethz.ch>
2021-04-23 22:20:50 +0200

    Added test phase

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ PATH_DATA_NEG = os.path.join(DIR_DATA, "train_neg_preprocessed.txt")
 PATH_DATA_POS = os.path.join(DIR_DATA, "train_pos_preprocessed.txt")
 DIR_PRETRAINED = "Pretrained/bert-base-uncased"
 N_EPOCHS = 3
-BATCH_SIZE = 16
-ACCUMULATION_SIZE = 16
+BATCH_SIZE_TRAIN = 16
+ACCUMULATION_SIZE_TRAIN = 16
+BATCH_SIZE_TEST = 16
 VERBOSE = True
 
 class EncodingsDataset(torch.utils.data.Dataset):
@@ -47,19 +48,26 @@ def main():
     #texts_pos = f_pos.readlines()
     #texts = texts_neg + texts_pos
     #labels = [0] * len(texts_neg) + [1] * len(texts_pos)
-    texts, labels = read_imdb_split(os.path.join(DIR_DATA, 'aclImdb/train'))
+    texts_train, labels_train = read_imdb_split(os.path.join(DIR_DATA, 'aclImdb/train'))
+    texts_test, labels_test = read_imdb_split(os.path.join(DIR_DATA, 'aclImdb/test'))
 
     if VERBOSE: print("loading tokenizer...")
     tokenizer = BertTokenizer.from_pretrained(DIR_PRETRAINED) 
 
     if VERBOSE: print("tokenizing data...")
-    encodings = tokenizer(texts, truncation=True, padding=True)
+    encodings_train = tokenizer(texts_train, truncation=True, padding=True)
+    encodings_test = tokenizer(texts_test, truncation=True, padding=True)
     
-    ds = EncodingsDataset(encodings, labels) 
-    dl = torch.utils.data.DataLoader(
-        dataset=ds,
-        batch_size=int(BATCH_SIZE/ACCUMULATION_SIZE),
+    ds_train = EncodingsDataset(encodings_train, labels_train) 
+    ds_test = EncodingsDataset(encodings_test, labels_test) 
+    dl_train = torch.utils.data.DataLoader(
+        dataset=ds_train,
+        batch_size=int(BATCH_SIZE_TRAIN/ACCUMULATION_SIZE_TRAIN),
         shuffle=True,
+    )
+    dl_test = torch.utils.data.DataLoader(
+        dataset=ds_test,
+        batch_size=BATCH_SIZE_TEST
     )
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -75,20 +83,34 @@ def main():
     for epoch in range(N_EPOCHS):
         if VERBOSE: print("epoch %d..." % epoch)
         avg_loss = 0.0
-        for i, batch in enumerate(dl):
+        for i, batch in enumerate(dl_train):
             input_ids = batch['input_ids'].to(device)
             attention_mask = batch['attention_mask'].to(device)
             labels = batch['labels'].to(device)
             outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
             loss = outputs[0]
-            loss /= ACCUMULATION_SIZE
+            loss /= ACCUMULATION_SIZE_TRAIN
             loss.backward()
             avg_loss += loss.item()
-            if (i + 1) % ACCUMULATION_SIZE == 0:
+            if (i + 1) % ACCUMULATION_SIZE_TRAIN == 0:
                 optimizer.step()
                 optimizer.zero_grad() 
                 print("avg. loss: %.3f" % avg_loss)
                 avg_loss = 0.0
+                
+    if VERBOSE: print("evaluation...")
+    model.eval()
+    count_correct = 0.0
+    with torch.no_grad():
+        for i, batch in enumerate(dl_test):
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+            outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+            preds = torch.argmax(outputs[1], dim=1)
+            count_correct += torch.sum(preds == labels).item()
+    accuracy = count_correct / len(dl_test)
+    print("accuracy: %.3f" % accuracy)
 
 if __name__ == "__main__":
     main()
