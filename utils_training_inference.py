184ad199f6c5342619ce5150277d352bec41b53a
haslebas <haslebas@ethz.ch>
2021-07-19 10:46:07 +0200

    undo comment-out mixed precision

--- utils_training_inference.py
+++ utils_training_inference.py
@@ -146,7 +146,7 @@ def training(model, dataloader_train, dataloader_test, fn_loss, device, args):
         None
     """
     optimizer = AdamW(model.parameters(), lr=5e-5)
-    # scaler = torch.cuda.amp.GradScaler(enabled=args.mixed_precision)
+    scaler = torch.cuda.amp.GradScaler(enabled=args.mixed_precision)
     for epoch in range(args.epochs):
         model.train()
         avg_loss = 0.0
@@ -155,16 +155,16 @@ def training(model, dataloader_train, dataloader_test, fn_loss, device, args):
             labels = batch["labels"]
             inputs = move_to_device(inputs, device)
             labels = move_to_device(labels, device)
-            # with torch.cuda.amp.autocast(enabled=args.mixed_precision):
-            preds = model(**inputs)
-            loss = fn_loss(preds, labels)
-            loss /= args.accumulation_size
-            # scaler.scale(loss).backward()
+            with torch.cuda.amp.autocast(enabled=args.mixed_precision):
+                preds = model(**inputs)
+                loss = fn_loss(preds, labels)
+                loss /= args.accumulation_size
+            scaler.scale(loss).backward()
             loss.backward()
             avg_loss += loss.item()
             if (i + 1) % args.accumulation_size == 0:
-                # scaler.step(optimizer)
-                # scaler.update()
+                scaler.step(optimizer)
+                scaler.update()
                 optimizer.step()
                 optimizer.zero_grad() 
                 if args.verbose:
